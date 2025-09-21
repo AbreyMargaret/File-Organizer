1fdceeed9515d08790a3800a9f2629f9cd19fba3
AbreyMargaret <barcellanoabrey@gmail.com>
2025-09-21 22:37:42 +0800

    Added display list of documents function

--- OrganizeF/FileOrganizer.py
+++ OrganizeF/FileOrganizer.py
@@ -1,12 +1,16 @@
 import os
 
 directory_path = r"C:\Users\barce\python-projects\OrganizeF\Files"
+directory_docs = r"C:\Users\barce\python-projects\OrganizeF\Files\Docs"
+
 
 all_entries = os.listdir(directory_path)
+all_docs = os.listdir(directory_docs)
 
 print("========= FILE ORGANIZER ==============")
 
 a = 1
+
 
 for folders in all_entries:
     print(f"{a}.) {folders}")
@@ -15,8 +19,13 @@ for folders in all_entries:
 enter = int(input("Select which folder: "))
 
 def select_folder(enter):
+    b = 1
     if enter == 1:
             print("Documents:")
+            for docs in all_docs:
+                   print(f"{b}.) {docs}")
+                   b =+ 1
+
     elif enter == 2:
             print("Images:")
     elif enter == 3:
@@ -27,6 +36,8 @@ def select_folder(enter):
 
 
 select_folder(enter)
+
+
 
 
 
